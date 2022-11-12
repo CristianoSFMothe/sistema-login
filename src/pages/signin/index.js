dceb79d5e2885c53490bdcbf465964ea4993d203
Cristiano da Silva Ferreira <68359459+CristianoSFMothe@users.noreply.github.com>
2022-11-12 16:44:30 -0300

    :rocket: feat: Pagina de signin

--- src/pages/signin/index.js
+++ src/pages/signin/index.js
@@ -1,7 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+import * as StyleSignin from './styles';
+import { Link, useNavigate } from 'react-router-dom';
+import useAuth from '../../hooks/userAuth';
 
-const Home = () => {
-  return <div id="home">Signin</div>
-}
+const Signin = () => {
+  const { signin } = useAuth();
+  const navigate = useNavigate();
 
-export default Home;
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+  const [error, setError] = useState('');
+
+  const handleLogin = () => {
+    if (!email | !senha) {
+      setError("Preencha todos os campos");
+      return;
+    }
+
+    const res = signin(email, senha);
+
+    if (res) {
+      setError(res);
+      return;
+    }
+
+    navigate("/home");
+  };
+
+  return (
+    <StyleSignin.Container>
+      <StyleSignin.Label>SISTEMA DE LOGIN</StyleSignin.Label>
+      <StyleSignin.Content>
+        <Input
+          id='email-input'
+          type='email'
+          placeholder='Digite seu E-mail'
+          value={email}
+          onChange={(e) => [setEmail(e.target.value), setError('')]}
+        />
+        <Input
+          id='paassword-input'
+          type='password'
+          placeholder='Digite sua Senha'
+          value={senha}
+          onChange={(e) => [setSenha(e.target.value), setError('')]}
+        />
+        <StyleSignin.labelError>{error}</StyleSignin.labelError>
+        <Button id='btn-login' Text="Entrar" onClick={handleLogin} />
+        <StyleSignin.LabelSignup>
+          Não tem uma conta?
+          <StyleSignin.Strong>
+            <Link id='link-signup' to="/signup">&nbsp;Registre-se</Link>
+          </StyleSignin.Strong>
+        </StyleSignin.LabelSignup>
+      </StyleSignin.Content>
+    </StyleSignin.Container>
+  );
+};
+
+export default Signin;
