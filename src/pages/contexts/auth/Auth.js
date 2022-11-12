b99f1880baa76bffda2ef118e86a9a498a665557
Cristiano da Silva Ferreira <68359459+CristianoSFMothe@users.noreply.github.com>
2022-11-12 14:17:44 -0300

    :rocket: feat: função para login

--- src/pages/contexts/auth/Auth.js
+++ src/pages/contexts/auth/Auth.js
@@ -1,8 +1,8 @@
-import { createContext, useEffect, useState } from 'react';
+import {createContext, useEffect, useState} from 'react';
 
 export const AuthContext = createContext({});
 
-export const AuthProvider = ({ children }) => {
+export const AuthProvider = ({children}) => {
     const [user, setUser] = useState();
 
     useEffect(() => {
@@ -18,6 +18,28 @@ export const AuthProvider = ({ children }) => {
             if (hasUser) setUser(hasUser[0]);
         }
     }, []);
+
+    const signin = (email, password) => {
+        const usersStorage = JSON.parse(localStorage.getItem('users_db'));
+
+        const hasUser = usersStorage?.filter((user) => user.email === email);
+
+        if (hasUser?.length) {
+            if (hasUser[0].email === email && hasUser[0].password === password) {
+                const token = Math.random().toString(36).substring(2);
+
+                localStorage.setItem('user_token', JSON.stringify({email, token}));
+
+                setUser({email, password});
+
+                return;
+            } else {
+                return 'E-mail ou senha incorretos';
+            }
+        } else {
+            return 'Usuário não cadastrado';
+        }
+    };
 
     return <AuthContext.Provider>{children}</AuthContext.Provider>;
 };
