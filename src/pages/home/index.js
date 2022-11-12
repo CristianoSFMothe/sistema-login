b4c57f7dfed59e1da3924199c56290ee00da63e0
Cristiano da Silva Ferreira <68359459+CristianoSFMothe@users.noreply.github.com>
2022-11-12 16:46:37 -0300

    :rocket: feat: Pagina de home

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import Button from "../../components/Button";
+import useAuth from "../../hooks/useAuth";
+import * as StyleHome from "./styles";
 
 const Home = () => {
-  return <div id="home">Home</div>
-}
+  const { signout } = useAuth();
+  const navigate = useNavigate();
+
+  return (
+    <StyleHome.Container>
+      <StyleHome.Title>Home</StyleHome.Title>
+      <Button Text="Sair" onClick={() => [signout(), navigate("/")]}>
+        Sair
+      </Button>
+    </StyleHome.Container>
+  );
+};
 
 export default Home;
